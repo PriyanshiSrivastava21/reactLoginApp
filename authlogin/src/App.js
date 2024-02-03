01ab175dc0331be7532dd575c0c1bf77eb92cb6f
Priyanshi Srivastava <priyanshi.srivastava@comviva.com>
2024-02-03 23:28:28 +0530

    try something else for fixing vercel issue

--- authlogin/src/App.js
+++ authlogin/src/App.js
@@ -25,24 +25,74 @@
 // export default App;
 
 // App.js
-import React from "react";
-import { Auth0Provider } from "@auth0/auth0-react"; // Import Auth0Provider
-
-import LoginForm from "./login";
-import LandingPage from "./landingPage";
+// LoginForm.js
+import React, { useState } from "react";
+import "./App.css"; // Import the CSS file
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loginWithRedirect, isAuthenticated, user, isLoading, logout } =
+    useAuth0();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your authentication logic here
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
   return (
-    <Auth0Provider
-      domain="dev-webindia.us.auth0.com"
-      clientId="JMDl15rDHfngfOnH3khsftZes8VLfzVz"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      {/* <LoginForm /> */}
-      <LandingPage />
-    </Auth0Provider>
+    <div className="page-container">
+      <form className="form-container" onSubmit={handleSubmit}>
+        {isAuthenticated ? (
+          <h1>Hi {user.name}..</h1>
+        ) : (
+          <h1>Hi User! Please Login for moving forward...</h1>
+        )}
+
+        {isAuthenticated && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        )}
+
+        <br />
+        {isAuthenticated ? (
+          <button
+            type="submit"
+            className="form-button"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+        ) : (
+          <>
+            <button
+              type="submit"
+              className="form-button"
+              onClick={() => loginWithRedirect()}
+            >
+              Login
+            </button>
+          </>
+        )}
+      </form>
+    </div>
   );
 };
 
